c6784aa1f8e9ca674502a52708354d8343f157bc
slopezelias <slopezelias@udesa.edu.ar>
2026-09-03 12:24:16 -0300

    delete tp1

--- tp1/testing.h
+++
@@ -1,8 +0,0 @@
-#ifndef TESTING_H
-#define TESTING_H
-
-#define test_assert(message, test_result) real_test_assert(message, test_result, __FILE__, __LINE__)
-
-bool real_test_assert(const char *message, bool test_result, const char *file, int line);
-
-#endif //TESTING_H
